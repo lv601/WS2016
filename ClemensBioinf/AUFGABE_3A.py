94843f1a0283ed28f5ddd269e334db4a731c1bd1
Vortex <c.spielvogel@ymail.com>
2016-08-29 19:50:20 +0200

    Aufgabe 3B

--- ClemensBioinf/AUFGABE_3A.py
+++ ClemensBioinf/AUFGABE_3A.py
@@ -2,12 +2,12 @@ from random import randint
 
 right = False
 guessCount = 0
-randomNum = randint(1,10)
+randomNum = randint(1, 10)
 print("Geben Sie eine Zahl zwischen 1 und 10 ein: ", end = "")
 while right == False:
     guess = int(input())
     guessCount += 1
-    if guess in range(1,11):
+    if guess in range(1, 11):
         if guess == randomNum:
             print("Richtige Zahl! Sie haben gewonnen.")
             right = True
@@ -17,6 +17,7 @@ while right == False:
             print("Zahl zu klein.")
     else:
         print("Die Zahl muss zwischen 1 und 10 sein.")
+
     if guessCount == 5 and right == False:
         print("Sie haben verloren.")
     elif guessCount < 5 and right == False:
