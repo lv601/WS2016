779b1c9fbafa7f6635f639d458271b460c4fc1c0
alex <ned_flanders@gmx.at>
2016-09-05 01:18:51 +0200

    add prior exercises

--- genio0815/3A.py
+++ genio0815/3A.py
@@ -7,29 +7,32 @@ tries = 3
 guesses = []
 
 for i in range(tries):
-    rate = input("Enter number between (inclusive) 1 and 10:  ")
+    rate = input("Enter number between (inclusive) 1 and %i:  " % upperBound)
 
-    if rate == str(guess):
-        print("Great, got it")
-        break
-    else:
-        try:
-            rate = int(rate)
+    try:
+
+        rate = int(rate)
+
+        if rate == guess:
+            print("Great, got it")
+            break
+        else:
+
             if (rate > upperBound):
-                print("enter a number between (inclusive) 1 and 10")
+                print("enter a number between (inclusive) 1 and %i:  " % upperBound)
 
             if rate in guesses:
                 print("dumb? tried this one already...")
-                print("so far entered:")
+                print("integers entered so far :")
                 for k in range(0, len(guesses)):
                     print(guesses[k])
 
             guesses.append(rate)
 
-        except ValueError:
-            print("input error, enter int like string!")
+    except ValueError:
+        print("input error, enter int like string!")
 
-        print("nope, tries left:", format(tries-i-1))
+    print("nope, tries left:", format(tries-i-1))
 
     if i == tries - 1:
         print("Sorry, lost")
