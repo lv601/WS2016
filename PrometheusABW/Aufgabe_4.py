bc9cba0eb3742cb64ef7254fdd75d7c415de4177
Michael Lehrach <michael.lehrach@me.com>
2016-09-05 20:42:34 +0200

    added Aufgabe4

---
+++ PrometheusABW/Aufgabe_4.py
@@ -0,0 +1,20 @@
+flaglist = []
+a=2
+b=4
+c=8
+d=16
+e=32
+f=64
+g=128
+h=256
+
+flags = a | b 
+
+print(flags)
+print(bin(flags))
+
+for i in range(8):
+	if flags & 2**i:
+		flaglist.append(2**i)
+		
+print("The flags are set:", flaglist)
