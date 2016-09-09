c51ed16fbd349bf3e62202d36788ff3f128363c9
frankruge <frankruge@gmail.com>
2016-09-09 17:07:05 +0200

    file 5b erstellt

---
+++ frankruge/Aufgabe5B.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3    # shebang needed in executeable files. chmod a+x myfile
+'''
+from random import randint
+randomNumber=randint(1,10)
+userNumber=11  #initialisierung der variable
+counter=0
+while(randomNumber != userNumber and counter < 10):
+	counter+=1
+	userNumber = int(input("geben sie eine Zahl zwischen 1 und 10  ein:"))
+	if(randomNumber == userNumber):
+		print("richtig!")
+		break
+	elif(randomNumber < userNumber):
+		print("falsch! zu hoch.")
+	else:
+		print("falsch! zu niedrig.")
+
+print("you used up "+str(counter)+ "of 10 trials")
+print("have a nice day")
+'''
+
+
+
+
+
+def guess_number(randomNumber, count):
+	count+=1
+	if(count==10):
+		return ("zu viele Versuche")
+	else:
+		userNumber = int(input("geben sie eine Zahl zwischen 1 und 10  ein:"))
+		if(randomNumber == userNumber):
+			print("richtig!")
+			return("yippie")
+		elif(randomNumber < userNumber):
+			print("falsch! zu hoch.")
+			guess_number(randomNumber, count)
+		else:
+			print("falsch! zu niedrig.")
+			guess_number(randomNumber, count)
+
+
+from random import randint
+randomNumber=randint(1,10)
+count=0
+guess_number(randomNumber, count)
+print("have a nice day")
+
+
