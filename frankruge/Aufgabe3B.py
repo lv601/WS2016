4487e0325a9969118d9b8b9436349ce42a69a24a
frankruge <frankruge@gmail.com>
2016-09-09 14:26:20 +0200

    file Aufgabe3B.py erstellt

---
+++ frankruge/Aufgabe3B.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3    # shebang needed in executeable files. chmod a+x myfile
+
+
+theString=input("geben sie einen string ein: \n")
+#print(len(theString))
+for i in range(len(theString)):
+	if(len(theString[i:i+3]) == 3):
+		print(" "*i+theString[i:i+3])
+	else:
+		break
+
