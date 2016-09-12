7fae55298310f75ba84365306f6cc11d631484d6
Shelley Mangan <mangan.shelley@gmail.com>
2016-09-12 20:11:50 +0200

    p.7

--- ShellMangan/MANGAN_p.7.py
+++ ShellMangan/MANGAN_p.7.py
@@ -3,9 +3,9 @@ menu=[("Suppe","Tagessuppe von vorvorgestern", "2.30"), ("Suppe deluxe", "Für d
 #header
 string=27*"*"+"Menu"+29*"*"+"\nVorspeisen:\n"
 #first
-string+=("{speisen[0][0]:<15}{speisen[0][1]:<30}{speisen[0][2]:<10} €\n").format(speisen=menu)
+string+=("{speisen[0][0]:<15}{speisen[0][1]:^30}{speisen[0][2]:>10} €\n").format(speisen=menu)
 #second
-string+=("{speisen[1][0]:<15}{speisen[1][1]:<30}{speisen[1][2]:<10} €\n").format(speisen=menu)
+string+=("{speisen[1][0]:<15}{speisen[1][1]:^30}{speisen[1][2]:>10} €\n").format(speisen=menu)
 #footer
 string+= "\n"+60*"*"+"\n"
 
