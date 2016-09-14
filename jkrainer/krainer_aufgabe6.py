21958862ea0603755947a30dcc1cfbb015dee6ab
Julie Krainer <julie.krainer@stud.fh-campuswien.ac.at>
2016-09-14 23:01:03 +0200

    Aufgaben hinzugefügt

--- jkrainer/krainer_aufgabe6.py
+++ jkrainer/krainer_aufgabe6.py
@@ -1,18 +1,19 @@
+from pprint import pprint
 file_handle = open("/home/julie/Desktop/FH/EPROG/examples/sequence.fasta", "r")
 
-db =[{}]
-
+db =[]
+a=0
+sequence_data = ''
+raw_data = ''
 for line in file_handle:
     if line.startswith(">"): #wenn das hier TRUE ist, dann wird ein neues Objekt erstellt und die anschließenden Sachen werden auf die weiteren Indices weiter gegeben
         description, id = line[1:].split(maxsplit=1) #aus beiden Teilen wird eine Variable erstellt; es wird genau einmal gesplittet
-        #print(description)
-        #print(id)
-        db = {"id":id, "description":description}
-        #print(db)
+        db.append({'id': id, 'description': description, 'sequence': "", 'raw': line})
     else:
-        raw = line
-        #print(raw) #dings.strip()
-        sequence = line.rstrip()
-        db["sequence"] = sequence
+        db[-1]['sequence'] += line.rstrip()
+        db[-1]['raw'] += line
+pprint(db)
 
-print(db)
+print(db[0])
+
+print(len(db))
--- jkrainer/krainer_aufgabe7.py
+++ jkrainer/krainer_aufgabe7.py
@@ -13,7 +13,3 @@ print()
 print(("Suppe").format(align="<"), ("Tagessuppe von vorvorgestern").format(align="^"), ("{euro}.{cent} €").format(euro="2",cent="30", align=">"))
 print(("Suppe deluxe\tFür die große Geldbörse\t{tausend}.{hundert},{cent} €").format(tausend="1", hundert="300", cent="50"))
 print(("").center(50, '*'))
-
-print("\n", "Anderer Versuch:", "\n")
-
-print(("Menü".center(70, '*')), "\n" ,("asdf").format(align="<"), "\n")
