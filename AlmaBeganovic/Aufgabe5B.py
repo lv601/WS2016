86d68052916afcb316ca396d9b12ccdc539795f6
Norbert Auer on hoth <norbert@derauer.net>
2016-09-15 16:13:36 +0200

    Füge Verbesserungsvorschläge für @AlmaBeganovic, @CBTJ, @ClemensBioinf hinzu

--- AlmaBeganovic/Aufgabe5B.py
+++ AlmaBeganovic/Aufgabe5B.py
@@ -1,28 +1,43 @@
 # coding=utf-8
 from random import randint
 
+# Nein, Sie sollten das ganze Spiel in eine Funnktion einbetten. Nur randint
+# einzubetten ist das gleiche wenn Sie gleich randint ausführen
+# Sie können 'bereich' und 'versuche' als parameter verwenden
 def run_game(bereich):
     zufazahl = randint(1, bereich)
-    return zufazahl
+    #return zufazahl
 
-bereich = input ("Definieren Sie den Bereich für die Zufallszahl zwischen 1 und n ein: ")
+    # Von @auer eingerückt
+    bereich = input ("Definieren Sie den Bereich für die Zufallszahl zwischen 1 und n ein: ")
 
-versuche = input ("Bitte geben Sie Anzahl von Versuchen ein: ")
+    versuche = input ("Bitte geben Sie Anzahl von Versuchen ein: ")
 
-print ("Bereich ist zwischen 1 und ", bereich)
-print ("Anzahl von Versuchen ist ", versuche)
+    print ("Bereich ist zwischen 1 und ", bereich)
+    print ("Anzahl von Versuchen ist ", versuche)
 
-zufal_global = run_game(bereich)
-#print (zufal_global)
+    #zufal_global = run_game(bereich)
+    #print (zufal_global)
 
-for i in range (1, versuche+1):
-    zahl = input("Bitte geben Sie eine Zahl aus dem Bereich: ")
-    if zahl == zufal_global:
-        print ("Zahl ist erraten", zufal_global)
-        break;
+    # TIPP: Das selbe bekommen Sie auch so
+    # for i in range (versuche):
+    for i in range (1, versuche+1):
+        zahl = input("Bitte geben Sie eine Zahl aus dem Bereich: ")
+        # Achtung: Sie vergleichen hier int mit str Zahl 5 != '5'
+        if zahl == zufal_global:
+            print ("Zahl ist erraten", zufal_global)
+            # TIPP: Hier können Sie gleich auch die Funktion verlassen anstratt
+            # nur die Schleife und gleich noch dazu einen Rückgabewert, wie die
+            # erratene Zahl oder None, wenn Sie nicht erraten wurde
+            # return zahl
+            break;
 
-    if zahl < zufal_global:
-        print ("Zahl ist kleiner als Zufallszahl")
+        if zahl < zufal_global:
+            print ("Zahl ist kleiner als Zufallszahl")
 
-    if zahl > zufal_global:
-        print ("Zahl ist grosser als Zufallszahl")
+        if zahl > zufal_global:
+            print ("Zahl ist grosser als Zufallszahl")
+
+    # TIPP: Sie können wenn die Schleife komplett durchlaufen ist und das Spiel
+    # verloren ist, noch eine 'else' Zweig noch einbauen, der nur dann
+    # ausgeführt wird. So können Sie z.B. noch eine GAME OVER Message ausgeben
--- ClemensBioinf/AUFGABE_5C.py
+++ ClemensBioinf/AUFGABE_5C.py
@@ -1,9 +1,17 @@
 def string_parts(start, end, step):
     print("Enter a string: ", end="")
+    # TIPP: Sie können input direkt einen String übergeben, der angezeigt werden
+    # soll
+    # string = input("Enter a string: ")
+    # ACHTUNG: passen sie auf das Sie keine Funktionsnamen überschreiben, wie
+    # str hier. Sie wären sehr verplüfft, wenn sie später vielleicht ein int
+    # Value in einen String konvertieren möchten str(10)
+    # id ist hier ein gern verwendeter Variabelname
     str = input()
     diff = end - start
     #print(len(str[start:end]))
     for codon in range(0, len(str)):
+        # Interessante denkweise aber würde nicht start und step reichen?        
         while len(str[start:end]) == diff:
             print(str[start:end])
             start += step
