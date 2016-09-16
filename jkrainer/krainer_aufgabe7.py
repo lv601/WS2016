2b494a554c66d47652c660bc9beb1db167dcc61c
Julie Krainer <julie.krainer@stud.fh-campuswien.ac.at>
2016-09-16 18:33:11 +0200

    Aufgabe 7 hinzugefügt

--- jkrainer/krainer_aufgabe7.py
+++ jkrainer/krainer_aufgabe7.py
@@ -1,3 +1,5 @@
+###Julie Krainer###
+
 ### Aufgabe 7: Menükarten erstellung mit Hilfe des Formatstrings
 
 #************* Menü ********************
@@ -6,10 +8,20 @@
 #Suppe deluxe        Für die große Geldbörse             1.300,50 €
 #************************************
 
+#erste Lösung#
 
-print(("Menü").center(50, '*'))
-print("Vorspeisen:")
-print()
+print((" Menü ").center(100, '*'))
+print("Vorspeisen:\n".ljust(100))
 print(("Suppe").format(align="<"), ("Tagessuppe von vorvorgestern").format(align="^"), ("{euro}.{cent} €").format(euro="2",cent="30", align=">"))
 print(("Suppe deluxe\tFür die große Geldbörse\t{tausend}.{hundert},{cent} €").format(tausend="1", hundert="300", cent="50"))
-print(("").center(50, '*'))
+print(("").center(100, '*'))
+
+print("\n")
+
+#Schönere Lösung#
+
+print((" Menü ").center(123, '*'))
+print("Vorspeisen:\n".ljust(120))
+print(("Suppe deluxe").ljust(50), ("Für die große Geldbörse").ljust(50), ("{tausend}.{hundert},{cent} €").format(tausend="1", hundert="300", cent="50").rjust(20))
+print(("Suppe").ljust(50), ("Tagessuppe von vorvorgestern").ljust(50), ("{euro}.{cent} €").format(euro="2",cent="30").rjust(20))
+print(("").center(123, '*'))
