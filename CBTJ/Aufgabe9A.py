fc860acd0f25b6c75332270389f9d824c069dc3c
Claudia Juno <junoclaudia@yahoo.de>
2016-09-25 13:47:13 +0200

    kleine Änderung in 9A

--- CBTJ/Aufgabe9A.py
+++ CBTJ/Aufgabe9A.py
@@ -17,12 +17,12 @@ def NeunA2 (s, repeat):
 
 
 start = time.time()
-a=NeunA1("Hope should be a controled substance", 1000000)
+a=NeunA1("Hope should be a controlled substance", 1000000)
 end = time.time()
 print("Dauer alte Methode: {:.3} seconds".format(end-start))  # 3 für 3 gültige Stellen?
 
 start = time.time()
-b=NeunA2 ("Hope should be a controled substance", 1000000)
+b=NeunA2 ("Hope should be a controlled substance", 1000000)
 end = time.time()
 print("Dauer neue Methode: {:.3} seconds".format(end-start))
 
