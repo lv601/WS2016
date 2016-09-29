d07bf72f1f283cc1123211d15ed8712cd7a12d6c
Eva Niessner <eva.niessner@chello.at>
2016-09-29 19:34:18 +0200

    File added

---
+++ evni/Aufgabe_7.py
@@ -0,0 +1,9 @@
+#####Aufgabe7#Speisekarte erstellen#####
+
+header = 27* '*' + 'Menü' + 27* '*'
+vorspeise = 'Vorspeisen: '
+suppe_1 = 'Suppe ' + 10*' ' + 'Tagessuppe von gestern' + 10*' ' + '2.30 Euro'
+suppe_2 = 'Suppe deluxe ' + 3*' ' + 'Tagessuppe Spezial' +13*' ' + '1,300 Euro'
+footer = 58* '*'
+
+print('\n', header, '\n', vorspeise, '\n', suppe_1, '\n', suppe_2, '\n', footer) 
