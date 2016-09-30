63d28fe9216550b313bdc4db324c37ad0935bfc5
Norbert Auer <norbert@derauer.net>
2016-09-30 12:36:40 +0200

    Update fasta_parser.py
    Unterstützt nun Unix-Pipes mit und ohne Angabe des pseudo Arguments
    
    Wandelt sys.stdin in den binär mode um

--- nauer/fasta_parser.py
+++ nauer/fasta_parser.py
@@ -6,18 +6,38 @@ from argparse import ArgumentParser as Parser
 from argparse import FileType
 import io
 
+# Run:
+# fasta_parser.py -i seq.fasta -o output
+# fasta_parser.py -i seq.fasta
+# fasta_parser.py # start terminal line input - type fasta seq and send stream with ctrl+d
+# cat seq.fasta | fasta_parser.py -o output
+# cat seq.fasta | fasta_parser.py > output
+
 parser= Parser()
 
-parser.add_argument("filename", help="Filename", type=FileType("rb"))
+# default="-" defines pseudoargument as default -> file stream -> sys.stdin or sys.stdout if mode "w"
+# pseudo element allows Unix piping -> cat sequence.fasta | fasta_parser.py works.
+# Because of default="-" no extra pseudo element is necessary
+parser.add_argument("-i","--input", help="Input filename", type=FileType("rb"), default="-")
+parser.add_argument("-o","--output", help="Output filename", type=FileType("w"), default="-")
 
 args = parser.parse_args()
 
-data = ws.parse_fasta(args.filename)
+# FileType("rb") or FileType("tb") with the pseudo argument returns the sys.stdin stream object
+# sys.stdin has both streams binary and Unicode but sys.stdin directly is Unicode. To get the binary
+# stream you have to reference sys.stdin.buffer
 
-print("Id\tGC%")
+# Test if input is sys.stdin and when switch stream to byte stream
+if args.input == sys.stdin:
+    args.input = args.input.buffer
+
+data = ws.parse_fasta(args.input)
+
+# Because I use streams it does not matter if stream is from file or from standard in/out
+print("Id\tGC%", file=args.output)
 
 for index, seq in enumerate(data):
-    print("{0}\t{1:.2f}".format(seq['id'].decode(), ws.get_gc_content(data, index)))
+    print("{0}\t{1:.2f}".format(seq['id'].decode(), ws.get_gc_content(data, index)), file=args.output)
 
 
 # print(ws.__name__, ws.__file__)
