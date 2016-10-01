727a867201c728906cf3b0b528c7f78e321a150a
Julie Krainer <julie.krainer@stud.fh-campuswien.ac.at>
2016-10-01 11:47:52 +0200

    Aufgaben hinzugefügt

---
+++ jkrainer/krainer_aufgabe9B.py
@@ -0,0 +1,74 @@
+import time, io
+
+def parse_fasta(file, db):
+    file_handle = open(file, "r")
+
+
+    for line in file_handle:
+        if line[0] == ">":
+            description, id = line[1:].split(maxsplit=1) #aus beiden Teilen wird eine Variable erstellt; es wird genau einmal gesplittet
+            db.append({'id': id,
+                       'description': description,
+                       'sequence': "",
+                       'raw': line})
+        else:
+            db[-1]['sequence'] += line.rstrip()
+            db[-1]['raw'] += line
+
+def parse_fasta_binary(file, db):
+    file_handle = open(file, "rb")
+
+    for line in file_handle:
+        if line[0] == 62:
+            description, id = line[1:].split(maxsplit=1) #aus beiden Teilen wird eine Variable erstellt; es wird genau einmal gesplittet
+            db.append({'id': id,
+                       'description': description,
+                       'sequence': bytearray(),
+                       'raw': bytearray()})
+        else:
+            db[-1]['sequence'] += line.rstrip()
+            db[-1]['raw'] += line
+
+def parse_fasta_io(file, db):
+    file_handle = open(file, "r")
+
+    for line in file_handle:
+        if line[0] == ">":
+            description, id = line[1:].split(maxsplit=1) #aus beiden Teilen wird eine Variable erstellt; es wird genau einmal gesplittet
+
+            db.append({'id': id,
+                       'description': description,
+                       'sequence': io.StringIO(),
+                       'raw': io.StringIO(line)})
+        else:
+            db[-1]['sequence'].write(line.rstrip())
+            db[-1]['raw'].write(line)
+
+
+def parse_fasta_binary_io(file, db):
+    file_handle = open(file, "rb")
+
+    for line in file_handle:
+        if line[0] == 62:
+            description, id = line[1:].split(maxsplit=1) #aus beiden Teilen wird eine Variable erstellt; es wird genau einmal gesplittet
+            db.append({'id': id,
+                       'description': description,
+                       'sequence': io.BytesIO(),
+                       'raw': io.BytesIO()})
+        else:
+            db[-1]['sequence'].write(line.rstrip())
+            db[-1]['raw'].write(line)
+        #pprint(db)
+
+def take_time(func, args, **kwargs): #**kwargs ist für das Dictionary (keyworded arguments)
+    start = time.time() #speichert die aktuelle Zeit in eine Variable (Sekunden seit epoch - 1.1.1970 0 Uhr 0:0)
+    func(args, **kwargs)
+    end = time.time()
+    print("Function", func, "takes {:.3} seconds".format(end-start)) #hier wird auf drei Nachkommastellen reduziert
+
+database = []
+
+take_time(parse_fasta, "../examples/long.fasta", db=database) #starte die Funktion parse_fasta innerhalb der Funktion take_time um die Zeit zu stoppen.
+take_time(parse_fasta_binary, "../examples/long.fasta", db=database)
+take_time(parse_fasta_io, "../examples/long.fasta", db=database)
+take_time(parse_fasta_binary_io, "../examples/long.fasta", db=database)
