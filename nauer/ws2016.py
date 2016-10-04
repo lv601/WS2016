e50412e9fbb17ae1ff4cee3519b531f864ddcac5
Norbert Auer <norbert@derauer.net>
2016-10-04 17:27:22 +0200

    Update ws2016.py

--- nauer/ws2016.py
+++ nauer/ws2016.py
@@ -1,76 +1,117 @@
 import time
 import sys
 
-def parse_fasta(stream, db=None):
-    if db is None:
-        db = []
 
-    # Test if stream from type binary
-    if isinstance(stream.read(0), (bytes, bytearray)):
-        for line in stream:
-            if line[0] == 62:  # ord(">") bytes are stored as numbers
-                # New fasta sequence starts - parse header
-                ident, sep, desc = line[1:].strip().partition(b" ")
+#from Exception import NotImplementedError
 
-                # Create new sequence entry in db
-                db.append({'id': ident,
-                         'description': desc,
-                         'sequence': bytearray(),  # Switch to bytearray
-                         'raw': bytearray(line)})  # Switch to bytearray
+class SeqRecord:
+    def __init__(self, id , seq, raw):
+        self.id = id
+        self.seq = seq
+        self.raw = raw
+
+
+class SeqRecordFasta(SeqRecord):
+    def __init__(self, id, seq, raw, description):
+        super().__init__(id, seq, raw)
+        self.desc = description
+
+
+class SeqRecordGenbank(SeqRecordFasta):
+    def __init__(self, id, seq, raw, description):
+        super().__init__(id, seq, raw, description)
+        pass
+
+
+class Parser:
+    def __init__(self):
+        self.records = []
+
+    def add_record(self, record):
+        self.records.append(record)
+
+        # returns the index of that record
+        return len(self.records) - 1
+
+    def parse(self, stream, type="fasta"):
+        # Test if stream from type binary
+        if isinstance(stream.read(0), (bytes, bytearray)):
+            if type == "fasta":
+                db = {}
+                for line in stream:
+                    if line[0] == 62:  # ord(">") bytes are stored as numbers
+                        if db:
+                            self.records.append(SeqRecordFasta(**db))
+                            db = {}
+
+                        # New fasta sequence starts - parse header
+                        ident, sep, desc = line[1:].strip().partition(b" ")
+
+                        db = {'id': ident,
+                              'description': desc,
+                              'seq': bytearray(),  # Switch to bytearray
+                              'raw': bytearray(line)}   # Switch to bytearray
+                    else:
+                        # Sequence line
+                        db['seq'] += (line.rstrip())
+                        db['raw'] += (line)
             else:
-                # Sequence line
-                db[-1]['sequence'] += (line.rstrip())
-                db[-1]['raw'] += (line)
-        return db
-    else:
-        print("Stream must be opened as binary", file=sys.stderr)
-        # Exit script with -1. Signals an error to the operationg system
-        exit(-1)
+                raise NotImplementedError
+        else:
+            print("Stream must be opened as binary", file=sys.stderr)
+            # Exit script with -1. Signals an error to the operationg system
+            exit(-1)
 
-def get_raw(db, index):
-    """
-    Return original text data
-    :param db:
-    :param index:
-    :return: str
-    """
-    return db[index]['raw']
+    def get_raw(self, index):
+        """
+        Return original text data
+        :param db:
+        :param index:
+        :return: str
+        """
+        return self.records[index]['raw']
 
-def get_fasta(db, index, line_length=80):
-    """
-    Return sequence in fasta format
-    :param db:
-    :param index:
-    :return: str
-    """
-    strings = [">{0[id]}|{0[description]}".format(db[index])]
+    def get_fasta(self, index, line_length=80):
+        """
+        Return sequence in fasta format
+        :param db:
+        :param index:
+        :return: str
+        """
+        print(self.records[0].desc, index)
+        strings = [">{0}|{1}".format(self.records[index].id.decode(), self.records[index].desc.decode())]
 
-    for i in range(0, len(db[index]['sequence']), line_length):
-        strings.append(db[index]['sequence'][i:i + line_length])
+        for i in range(0, len(self.records[index].seq), line_length):
+            strings.append(self.records[index].seq[i:i + line_length].decode())
+        return "\n".join(strings)
 
-    return "\n".join(strings)
+    def add_sequence_object(self, id, description, sequence, **features):
+        """
+        Add a new sequence to data dictionary
+        :param db:
+        :param id:
+        :param description:
+        :param sequence:
+        :param features: Any kind of feature not mentioned in the main features
+        :return: None
+        """
+        self.records.append({'id': id, 'description': description, 'sequence': sequence, **features})
 
-def add_sequence_object(db, id, description, sequence, **features):
-    """
-    Add a new sequence to data dictionary
-    :param db:
-    :param id:
-    :param description:
-    :param sequence:
-    :param features: Any kind of feature not mentioned in the main features
-    :return: None
-    """
-    db.append({'id': id, 'description': description, 'sequence': sequence, **features})
+    def get_gc_content(self, index):
+        """
+        Returns the % GC content
+        :param db:
+        :param index:
+        :return: float
+        """
+        seq = self.records[index].seq.lower()
+        return 100 / len(seq) * (seq.count(b"g") + seq.count(b"c"))
 
-def get_gc_content(db, index):
-    """
-    Returns the % GC content
-    :param db:
-    :param index:
-    :return: float
-    """
-    seq = db[index]['sequence'].lower()
-    return 100 / len(seq) * (seq.count(b"g") + seq.count(b"c"))
+#rec = SeqRecordGenbank("id1112", "ATTGCCGTT")
+
+
+
+
 
 # Stop time of function calls
 def stop_time(func, *args, **kargs):
@@ -83,20 +124,24 @@ def stop_time(func, *args, **kargs):
     return result
 
 
-# Test if library have been imported or run directly 
+# Test if library have been imported or run directly
 if __name__ == "__main__":
     # Run test code
     print(__file__)
     print(__name__)
 
-    import io
+    parser = Parser()
 
-    b_io = io.BytesIO(b">Header\nATGCTTGTGTC")
-    data = []
-    parse_fasta(b_io, data)
-    print(data)
-    print("GC-Content: {:.2f}%".format(get_gc_content(data, 0)))
+    with open("../examples/sequence.fasta", "rb") as f:
+        parser.parse(f, type="fasta")
 
+        print(parser.records[1].id)
+        print(parser.records[2].id)
+        print(parser.records[3].id)
+
+        print(parser.get_fasta(1))
+
+        print(parser.get_gc_content(1))
 
 
 
