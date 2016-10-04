41a05ebe114a0207ea01fdf28a3d33bc49c1cc8e
alex <ned_flanders@gmx.at>
2016-10-04 03:34:06 +0200

    6.py v0

--- genio0815/6.py
+++ genio0815/6.py
@@ -1,33 +1,27 @@
 
 # db [{seq1}, {seq2}, {seq3}, ...] Sequence of dictionaries...acc requirements
 
+import re
+
 def parseFastaFile(db,file):
 
     """
-    Parse a multi sequence fasta file and map it to a dictionary
+    Parse a multi sequence fasta file and map it to a dictionary...acc requirements
     :param db:
-    :param file_name:
+    :param file:
     :return: None
     """
 
     file_handler = open(file, 'r')
 
     for line in file_handler:
-        if zeile[0] == ">":
-            db['id'] = line.split()[0][1:]
-            db['description'] = line.split(maxsplit=1)[1].strip()  # .rstrip('\n')
-            db['sequence'] = ""
-            db['raw'] = line.rstrip('\n')
-            db.append(seq_dict)
+        if line[0] == ">":
+            fastaId = line.split()[0][1:]
+            desc = line.split(maxsplit=1)[1].strip()
+            db.append({'id': fastaId, 'description': desc, 'sequence': "", 'raw': line})
         else:
-            str1 = line.strip('\n')
-            db[len(seq_list)-1]['sequence'] += str1
-            db[len(seq_list)-1]['raw'] += str1
-
-result = parseFastaFile('../examples/sequence.fasta')
-
-for i in range(0, len(result)):
-    print(result[i]['description'])
+            db[len(seq_list)-1]['sequence'] += line.rstrip()
+            db[len(seq_list)-1]['raw'] += line
 
 def get_raw(db,index):
     return db[index]['raw']
@@ -54,12 +48,47 @@ def get_fasta(db,index):
     fasta += seq
     return fasta
 
-def get_feature(db, index, feature):
+#########
+
+def parseGb(db, file):
+    """
+    Parse a multi sequence genbank file and map it to a dictionary
+    The main loop runs first. It seperates the genbank sequences. Then the secondary loop seperates different fields in
+    the genbank sequence
+    :param db:
+    :param file_name:
+    :return: None
+    """
+    rawData = []
+    raw = ""
+
+    with open(file,'r') as inFile:
+        for line in inFile:
+            if line.startswith("//"):
+                raw += 'DONE'  # end token for regex
+                rawData.append(raw)
+                raw = line
+            else:
+                raw += line
+
+    gbStructure = [('id', 'ACCESSION', 'VERSION'), ('description', 'DEFINITION', 'ACCESSION'),
+                 ('ORIGIN', 'ORIGIN', 'DONE'), ('FEATURES', 'FEATURES', 'ORIGIN'),
+                 ('organism', 'ORGANISM', 'COMMENT')]
+
+    #features = []
+
+    for i in range(len(rawData)):
+        for dbId, fromStr, toStr in gbStructure:
+            #db[-1][dbId] = re.search(fromStr + r'(.*?)' + toStr, rawData[i],re.DOTALL).group(1)
+            aux = re.search(fromStr + r'(.*?)' + toStr, rawData[i], re.DOTALL).group(1)
+            print("for {} found {}".format(dbId, aux))
+
+    return None
+
+test = dict()
+testfile = "../examples/sequence.gb"
+parseGb(test, testfile)
 
 
 
 
-
-
-
-#def parseDB(db, inFile):
