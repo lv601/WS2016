36a81c7ad4a21e5dfb7e032d0462558854080193
Vortex <c.spielvogel@ymail.com>
2016-10-20 17:51:04 +0200

    auto commit

--- ClemensBioinf/AUFGABE_14.py
+++ ClemensBioinf/AUFGABE_14.py
@@ -1,2 +1,16 @@
-#siehe ws2016
+# Equivalent zu Aufgabe 11 fuer Parser-Klasse statt Parser-Funktion
+import ws2016
+import sys
 
+file = open(sys.argv[1],'rb')
+parser = ws2016.Parser(file.read())
+data = parser.fasta_parser()
+
+# Falls kein Index angegeben, wird erstes Fasta-File prozessiert
+try:
+    index = int(sys.argv[2])
+except:
+    index = 0
+
+# Gewuenschte Funktion muss hier eingegeben werden
+print('{}: {}'.format(parser.get_id(data,index), parser.get_gc_content(data,index)))
