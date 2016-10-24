20a1b18a09ec55de5f29cf15b7596c89af43a73e
Vortex <c.spielvogel@ymail.com>
2016-10-24 18:00:29 +0200

    auto commit

--- ClemensBioinf/AUFGABE_15.py
+++ ClemensBioinf/AUFGABE_15.py
@@ -1,21 +1,16 @@
-def fasta_parser(n):
-    data = []
+def fasta_parser():
     total_raw = ''
     for line in open('../examples/sequence.fasta', 'r'):
         total_raw += line
 
-    i = 1
-    while i < n:
-        for sequence in total_raw.split('>')[1:]:
-            header = sequence.partition(' ')
-            curr_seq = {}
-            curr_seq['raw'] = sequence
-            curr_seq['sequence'] = ''.join(sequence.splitlines()[3:])
-            curr_seq['id'] = header[0]
-            curr_seq['description'] = header[2].split('\n')[0]
-            data.append(curr_seq)
-        yield data[i + 1]
-        i += 1
+    for sequence in total_raw.split('>')[1:]:
+        header = sequence.partition(' ')
+        curr_seq = {}
+        curr_seq['raw'] = sequence
+        curr_seq['sequence'] = ''.join(sequence.splitlines()[3:])
+        curr_seq['id'] = header[0]
+        curr_seq['description'] = header[2].split('\n')[0]
+        yield curr_seq
 
-for result in fasta_parser(5):
+for result in fasta_parser():
     print(result['id'],result['description'])
