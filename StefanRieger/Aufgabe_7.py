4a6b1ffcff1c77f971d748c363e1dea465862c06
Stefan Rieger <rieger.stefan@gmx.net>
2016-10-24 13:15:41 +0200

    update

---
+++ StefanRieger/Aufgabe_7.py
@@ -0,0 +1,7 @@
+speisekarte = "Menu"
+print (speisekarte.center (60, "*"))
+print ('{:<20}' .format('Vorspeise:'))
+print ('{:<20}' '{:<20}' '{:>20} €'.format('Suppe','Suppe von vorgestern', 2.20))
+print ('{:<20}' '{:<20}' '{:>20} €'.format('Suppe de luxe','Fürs große Börserl', 999.99))
+Ende = ""
+print (Ende.center (60, "*"))
