0c42002999b237654e1ebddc9d411db81a3ad69f
Michael Lehrach <michael.lehrach@me.com>
2016-10-24 20:59:49 +0200

    GAMEPLAY_UNO angelegt

---
+++ uno/CODE/Gamplay_Uno.py
@@ -0,0 +1,25 @@
+# to handle gameplay of card playing games
+
+from random import randint
+import Gameplay, Card_Deck
+
+class Gameplay_Uno:
+    def __init__(self):
+        gameplay.__init__.super()
+
+    def draw_cards(self, available_card, number, draw_cards=[]):
+            while number_to_draw <= number
+            number_to_draw += 1
+            random_number = randint(min(index(available_card)), max(index(available_card)))
+            draw_cards = available_card[random_number].append
+            available_card[random_number].pop
+
+            return draw_cards
+
+        number_of_cards
+        get_first_card()
+        card_deck = Card_Deck_Uno
+        rules = Rules_Uno()
+
+if __name__ == "__main__":
+    gameplay()
