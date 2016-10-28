e27d3856cf68d448d11720dca398223e00b98f19
Michael Lehrach <michael.lehrach@me.com>
2016-10-28 17:06:03 +0200

    update libary

--- uno/CODE/Gamplay_Uno.py
+++ uno/CODE/Gamplay_Uno.py
@@ -1,28 +1,30 @@
 # to handle gameplay of card playing games
 
 from random import randint
-import Gameplay, Card_Deck, Card_Deck_Uno
+import Gameplay, Card_Deck, Card_Deck_Uno, Player, Rules_Uno
 
-class Gameplay_Uno:
+class Gameplay_Uno(Gameplay):
     def __init__(self):
-        gameplay.__init__.super()
         self.rules = Rules_Uno.rules()
         self.card_deck = Card_Deck_Uno.create_deck()
+        self.rules = Rules_Uno()
 
     def draw_cards(self, available_card, number, draw_cards=[]):
-            while number_to_draw <= number
-            number_to_draw += 1
-            random_number = randint(min(index(available_card)), max(index(available_card)))
-            draw_cards = available_card[random_number].append
-            available_card[random_number].pop
+        while number_to_draw <= number
+        number_to_draw += 1
+        random_number = randint(min(index(available_card)), max(index(available_card)))
+        draw_cards = available_card[random_number].append
+        available_card[random_number].pop
 
-            return draw_cards
+        return draw_cards
 
     def get_firtst_card(self,available_card):
         return  Gameplay_Uno.draw_cards(available_card, 1)
 
 
-        number_of_cards
+    def Number_of_cards(self, num_of_cards=[]):
+        for Player.other_player:
+            num_of_cards.append(Player.cards)
 
 
 
