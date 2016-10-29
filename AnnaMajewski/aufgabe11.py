66fa42003c6a27bb57c842d8323b633d917918e1
Anna Majewski <anna.majewski@stud.fh-campuswien.ac.at>
2016-10-29 18:34:23 +0200

    Aufgaben hochgeladen. Endlich!

--- AnnaMajewski/aufgabe11.py
+++ AnnaMajewski/aufgabe11.py
@@ -7,11 +7,12 @@
 import ws2016 as ws
 import sys, io
 
-if len(sys.argv) > 1: #wenn ich einen filename eingegeben habe, ist er in sys.argv[1]
-    filename = io.open(sys.argv[1], mode="rb") #oeffne file als bytes.IO
-    file = ws.fasta_parser(filename) #Aufruf des Parsers
-    print(file) #Ausgabe der Ausgabe vom Fasta Parser
-    filename.close() #Schliessen des Streams
+def parse_console():
+    if len(sys.argv) > 1: #wenn ich einen filename eingegeben habe, ist er in sys.argv[1]
+        filename = io.open(sys.argv[1], mode="rb") #oeffne file als bytes.IO
+        file = ws.fasta_parser(filename) #Aufruf des Parsers
+        print(file) #Ausgabe der Ausgabe vom Fasta Parser
+        filename.close() #Schliessen des Streams
 
 #stream = io.open("../examples/long.fasta", mode="rb")
 #ws.fasta_parser(stream)
--- AnnaMajewski/aufgabe16.py
+++ AnnaMajewski/aufgabe16.py
@@ -1,6 +1,6 @@
-## Date: 03.10.2016
+## Date: 20.10.2016 - 29.10.2016
 ## Author: Anna Majewski
-## Description: Einführung in Klassen
+## Description: Magic Methods zu Aufgabe 13 hinzufuegen
 
 class Eintrag: # Eine Klasse Eintrag wird erstellt
     def __init__(self, name, surname, hobbies, age, attributes): #enthaelt diese 5 Felder
@@ -9,6 +9,17 @@ class Eintrag: # Eine Klasse Eintrag wird erstellt
         self.hobbies = hobbies
         self.age = age
         self.attributes = attributes
+
+    def __str__(self):
+        return "Der Name des Benutzers ist " + self.name + " " + self.surname
+    # Verhalten der Klasse fuer str() geaendert.
+
+    def __bytes__(self):
+        return b"Das ist der Byte Mode" # Ausgabe bei bytes() auf Instanzen der Klasse
+
+    def __repr__(self):
+        return "Der Name des Benutzers ist " + self.name + " " + self.surname
+    # Verhalten der Klasse fuer repr() geaendert, gleich zu __str__
 
 class Adressbuch: # Eine Klasse Adressbuch wird erstellt
     def __init__(self):
@@ -20,11 +31,11 @@ class Adressbuch: # Eine Klasse Adressbuch wird erstellt
     def get_entry(self, index): # Methode Eintrag auslesen wird erstellt
         return self.data[index]
 
+## Eintrag mit Append eingefügt.
+
 #eintrag1 = Eintrag("Hans", "Gruber", "Raub und Diebstahl", 35, "ungeduldig, schiesswuetig, flugunfaehig")
 
-## Testen der Angaben
-#ad = Adressbuch()
-#ad.add_entry(eintrag1)
-#blabla = ad.get_entry(0)
-#print(blabla.__dict__)
-#print(eintrag1.hobbies)
+#buch = Adressbuch()
+#buch.add_entry(eintrag1)
+#str(eintrag1)
+#print(bytes(eintrag1))
