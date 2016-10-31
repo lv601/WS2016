3140e2a8bcbba73b911e22ec2fd71fb1f236ae0f
Anna Majewski <anna.majewski@stud.fh-campuswien.ac.at>
2016-10-31 02:22:25 +0100

    Added description

--- uno/CODE/Players_Uno.py
+++ uno/CODE/Players_Uno.py
@@ -1,12 +1,16 @@
-user_list=[]  #in case of deleting list upon add_new_player
+## Date: 20.10.2016 -
+## Authors: Alma Beganovic, Claudia Juno, Anna Majewski, Shelley Mangan
+## Description: Player Module for UNO game project
+
+user_list = []  #in case of deleting list upon add_new_player
 class Player:
     #Attributes: name ip-address cards other_player next_player
     def __init__(self, player, ip, cards, next_user, other_user):
-        self.name=name
-        self.ip=ip
-        self.cards=cards  #cards in the hand
-        self.next_user=next_user
-        self.other_user=other_user
+        self.name = name
+        self.ip = ip
+        self.cards = cards  #cards in the hand
+        self.next_user = next_user
+        self.other_user = other_user
 
     def call_attr_msg(self, ip, cards):
         #talk with server, messages required
@@ -14,7 +18,7 @@ class Player:
 
     def ask_name(self, player):
         #unique name!
-        player=input("Please enter your user name: ")
+        player = input("Please enter your user name: ")
         while player in user_list:
            print("This name has been taken.")
            self.ask_name
@@ -28,5 +32,5 @@ class Player:
         else:
             user_list.append
 
-if __name__=="__main__":
+if __name__ == "__main__":
     print(__file__)
